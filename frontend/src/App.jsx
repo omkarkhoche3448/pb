0218d52946eb730ebd6170905039d62bd05879e3
omkarkhoche3448 <omkarkhoche3448@gmail.com>
2024-12-10 14:38:47 +0530

    Move backend files to 'backend' folder and set up frontend structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-5">
+      <h1 className="text-8xl font-bold">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#1DA1F2',
+      },
+    },
+  },
+  plugins: [],
+}
